bd9ece8a047c12276863aea7ac61cedc1a3dc49c
celsius narhwal <hello@celsiusnarhwal.dev>
2025-06-06 15:20:09 -0400

    Rename `RILPlugin` to `SVGRPlugin`

--- RIL/plugins/svgr.py
+++ RIL/plugins/svgr.py
@@ -1,29 +1,10 @@
 import string
-import sys
 from typing import Unpack
 
 import json5 as json
-from loguru import logger
 from reflex.plugins import CommonContext, Plugin, PreCompileContext
 
-__all__ = ["RILPlugin"]
-
-
-def _configure_logging():
-    from reflex.config import get_config
-
-    log_level = get_config().loglevel
-
-    if log_level.casefold() == "default":
-        log_level = "warning"
-
-    logger.remove()
-    logger.add(
-        sink=sys.stderr,
-        level=log_level.upper(),
-        colorize=True,
-        format="<lvl>[Reflex Icon Library] {level}: {message}</>",
-    )
+__all__ = ["SVGRPlugin"]
 
 
 def _update_next_config(next_config_contant: str):
@@ -52,12 +33,11 @@ def _update_next_config(next_config_contant: str):
     return f"module.exports = {json.dumps(next_config)}"
 
 
-class RILPlugin(Plugin):
+class SVGRPlugin(Plugin):
     def get_frontend_development_dependencies(
         self, **context: Unpack[CommonContext]
     ) -> list[str] | set[str] | tuple[str, ...]:
         return ["@svgr/webpack"]
 
     def pre_compile(self, **context: Unpack[PreCompileContext]) -> None:
-        context["add_save_task"](_configure_logging)
         context["add_modify_task"]("next.config.js", _update_next_config)
